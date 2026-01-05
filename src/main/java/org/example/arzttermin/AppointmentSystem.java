3711042e62468c484672fd544ef4632d9a967916
Khalifeh <mhdeid12@gmail.com>
2026-01-05 23:10:00 +0100

    AppointmentSystem

---
+++ src/main/java/org/example/arzttermin/AppointmentSystem.java
@@ -0,0 +1,25 @@
+package org.example.arzttermin;
+
+import org.example.arzttermin.Model.SingletonAppointmentSystem;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class AppointmentSystem extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        SingletonAppointmentSystem.getInstance();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(AppointmentSystem.class.getResource ("view/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        stage.setTitle("PraxPlan");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
