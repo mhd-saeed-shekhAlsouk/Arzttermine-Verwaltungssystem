dba9abf846a850285ad59a0a0db8fe54a86d64b7
Khalifeh <mhdeid12@gmail.com>
2026-01-12 22:17:58 +0100

    Geschäftslogik : Docktor

--- src/main/java/org/example/arzttermin/Model/Doctor.java
+++ src/main/java/org/example/arzttermin/Model/Doctor.java
@@ -6,9 +6,9 @@ import java.sql.Date;
 public class Doctor extends User{
     public Doctor(int id, String firstName, String lastName, String username, String password, Date dob, String email, String gender, boolean isActive) {
         super(id, firstName, lastName, username, password, "Doctor", dob, email, gender, isActive);
-        this.setAvailability(new ArrayList<>());
+        this.setAvailablility(new ArrayList<>());
         for (int i = 1; i < 30; i++){
-            getAvailability().add(new AvailabilityCalendar(i + "-01-2025"));
+            getAvailablility().add(new AvailabilityCalendar(i + "-01-2025"));
         }
 
     }
