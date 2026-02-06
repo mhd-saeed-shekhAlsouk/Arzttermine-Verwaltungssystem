8545ed48a50421aa93f8d9750a7a7ef18656ee9e
Khalifeh <mhdeid12@gmail.com>
2026-02-07 00:47:29 +0100

    Model: Anpassungen

--- src/main/java/org/example/arzttermin/Model/Patient.java
+++ src/main/java/org/example/arzttermin/Model/Patient.java
@@ -6,4 +6,8 @@ public class Patient extends User{
     public  Patient(int id, String firstName, String lastName, String username, String password, Date dob, String email, String gender, boolean isActive) {
         super(id, firstName, lastName, username, password, "Patient", dob, email, gender, isActive);
     }
+    public Patient(int id) {
+        super(id, "", "", "", "", "Patient", new Date(2024, 10, 2), "", "", true);
+    }
 }
+
